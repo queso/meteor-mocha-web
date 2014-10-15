156285bef49d97ac18f82e6e5c3adb937c0c284e
Josh Owens <joshua.owens@gmail.com>
2014-10-15 08:43:03 -0400

    Add the debugOnly flag and switch the api calls to be camelCased.

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 Package.describe({
   name: 'mike:mocha',
   summary: "Run mocha tests in the browser",
-  version: "0.3.12",
+  version: "0.4.0",
+  debugOnly: true,
   git: "https://github.com/mad-eye/meteor-mocha-web"
 });
 
@@ -15,15 +16,15 @@ Npm.depends({
 //Require npm assertion library if it doesn't exist..
 //Npm.depends({chai: "1.9.0"});
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
   api.use(['velocity:core@0.2.14'], "server");
   api.use(['velocity:html-reporter@0.2.3'], "client");
   api.use(['templating@1.0.6'], "client");
 
-  api.add_files(["reporter.js", "server.js"], "server");
-  api.add_files(["client.html", "mocha.js", "reporter.js", "client.js", "chai.js"], "client");
+  api.addFiles(["reporter.js", "server.js"], "server");
+  api.addFiles(["client.html", "mocha.js", "reporter.js", "client.js", "chai.js"], "client");
 
-  api.add_files(["sample-tests/client.js","sample-tests/server.js"], "server", {isAsset: true});
+  api.addFiles(["sample-tests/client.js","sample-tests/server.js"], "server", {isAsset: true});
 
   api.export("MochaWeb", ["client", "server"]);
   api.export("MeteorCollectionTestReporter", ["client", "server"]);
